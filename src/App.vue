c8137f54e195bf1f221ac685fb504f3709106a48
Jose Ortega <joseortega@joseortega.es>
2019-10-29 00:45:18 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="app" class="antialiased text-gray-900 flex p-4 flex flex-wrap mx-auto lg:mx-32 xl:mx-64">
+    <div
+      v-for="item in data"
+      :key="item.imageUrl"
+      class="bg-gray-200 sm:px-2 lg:px-2 pt-2 pb-4 w-full md:w-1/2 lg:w-1/3"
+    >
+      <card-component :data="item"></card-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardComponent from "./components/CardComponent";
+export default {
+  name: "app",
+  components: { CardComponent },
+  data() {
+    return {
+      data: [
+        {
+          imageUrl: "https://source.unsplash.com/KJFKCed0YKo/1600x900",
+          imageAlt: "",
+          beds: 4,
+          baths: 2,
+          title: "Acogedora vivienda en pleno centro",
+          // priceInCents: 190000,
+          formattedPrice: "1.450€",
+          reviewCount: 14,
+          rating: 3.8,
+          isNew: true
+        },
+        {
+          imageUrl: "https://source.unsplash.com/tHkJAMcO3QE/1600x900",
+          imageAlt: "",
+          beds: 2,
+          baths: 1,
+          title: "Las mejores vistas de la ciudad",
+          // priceInCents: 190000,
+          formattedPrice: "1.900€",
+          reviewCount: 38,
+          rating: 4.2,
+          isNew: false
+        },
+        {
+          imageUrl: "https://source.unsplash.com/BZdv_I1LeLg/1600x900",
+          imageAlt: "",
+          beds: 4,
+          baths: 2,
+          title: "Apartamento con los mejores servicios",
+          // priceInCents: 190000,
+          formattedPrice: "1.200€",
+          reviewCount: 24,
+          rating: 4.5,
+          isNew: true
+        },
+        {
+          imageUrl: "https://source.unsplash.com/GZ5cKOgeIB0/1600x900",
+          imageAlt: "",
+          beds: 2,
+          baths: 1,
+          title: "Acogedora vivienda. Ideal parejas.",
+          // priceInCents: 190000,
+          formattedPrice: "1.250€",
+          reviewCount: 14,
+          rating: 4.8,
+          isNew: false
+        },
+        {
+          imageUrl: "https://source.unsplash.com/zLT3VqWEgOQ/1600x900",
+          imageAlt: "",
+          beds: 3,
+          baths: 1,
+          title: "Disfruta de la comodidad con estilo",
+          // priceInCents: 190000,
+          formattedPrice: "2.300€",
+          reviewCount: 78,
+          rating: 2.9,
+          isNew: true
+        },
+        {
+          imageUrl: "https://source.unsplash.com/9uH-hM0VwPg/1600x900",
+          imageAlt: "",
+          beds: 4,
+          baths: 2,
+          title: "Céntrica vivienda de diseño",
+          // priceInCents: 190000,
+          formattedPrice: "3.200€",
+          reviewCount: 124,
+          rating: 4.9,
+          isNew: false
+        },
+        {
+          imageUrl: "https://source.unsplash.com/akz0w36DpM4/1600x900",
+          imageAlt: "",
+          beds: 3,
+          baths: 2,
+          title: "Luminoso apartamento recien reformado",
+          // priceInCents: 190000,
+          formattedPrice: "1.050€",
+          reviewCount: 109,
+          rating: 3.5,
+          isNew: true
+        },
+        {
+          imageUrl: "https://source.unsplash.com/kdwahpWYfQo/1600x900",
+          imageAlt: "",
+          beds: 5,
+          baths: 2,
+          title: "La casa familiar que estabas buscando",
+          // priceInCents: 190000,
+          formattedPrice: "2.900€",
+          reviewCount: 58,
+          rating: 4.7,
+          isNew: false
+        },
+        {
+          imageUrl: "https://source.unsplash.com/FBXuXp57eM0/1600x900",
+          imageAlt: "",
+          beds: 4,
+          baths: 2,
+          title: "Espaciosa vivienda a 5 minutos de la playa",
+          // priceInCents: 190000,
+          formattedPrice: "2.200€",
+          reviewCount: 74,
+          rating: 4.1,
+          isNew: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style src="./assets/tailwind.css">
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bg-white rounded-lg overflow-hidden shadow-lg border border-gray-200">
+    <div class="relative bg-white pb-2/3">
+      <img
+        class="absolute top-0 w-full h-full object-cover object-bottom"
+        :src="data.imageUrl"
+        :alt="data.imageAlt"
+      />
+    </div>
+    <div class="p-6">
+      <div class="flex items-baseline">
+        <span
+          v-if="data.isNew"
+          class="text-xs font-semibold uppercase tracking-wide text-teal-700 bg-teal-200 rounded-full px-2 inline-block"
+        >Novedad</span>
+        <div
+          class="text-xs text-gray-600 uppercase tracking-wide ml-1"
+        >{{ data.beds }} dormitorios &bull; {{ data.baths }} baños</div>
+      </div>
+      <h4 class="text-lg font-semibold leading-tight truncate mt-1">{{ data.title }}</h4>
+      <div class="mt-1">
+        {{ data.formattedPrice }}
+        <span class="text-gray-600 text-sm">/ semana</span>
+      </div>
+      <div class="mt-2 flex items-center">
+        <svg
+          v-for="i in 5"
+          :key="i"
+          :class="i <= data.rating ? 'text-teal-500' : 'text-gray-300'"
+          class="h-4 w-4 fill-current"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M8.133 20.333c-1.147.628-2.488-.387-2.269-1.718l.739-4.488-3.13-3.178c-.927-.943-.415-2.585.867-2.78l4.324-.654 1.934-4.083a1.536 1.536 0 0 1 2.804 0l1.934 4.083 4.324.655c1.282.194 1.794 1.836.866 2.78l-3.129 3.177.739 4.488c.219 1.331-1.122 2.346-2.269 1.718L12 18.214l-3.867 2.119z"
+            fill-rule="evenodd"
+          />
+        </svg>
+        <span class="text-gray-600 text-sm ml-1">{{ data.reviewCount }} reseñas</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card-component",
+  props: ["data"]
+};
+</script>
+
+<style>
+@import url(../assets/tailwind.css);
+.pb-2\/3 {
+  padding-bottom: 66%;
+}
+</style>
+
